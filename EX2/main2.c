3eafff85ce580fa701f0c33af641991d1afe6f82
MaybeCandy01 <gary15878@gmail.com>
2025-09-18 15:40:16 +0800

    Update main2.c

--- EX2/main2.c
+++ EX2/main2.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
 
+int main(){
+	char a = 5;
+	printf("\t%d\n", a^1);//XOR
+	printf("\t%d\n", ~a);//NOT
+	printf("\t%d\n", a>>1);//右移
+	printf("\t%d\n", a<<1);//左移 
+
+	return 0;
+}
