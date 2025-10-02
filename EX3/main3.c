f4c698ca9e62dd5573fd6cc612b96cb3c6bd0fdf
MaybeCandy01 <gary15878@gmail.com>
2025-10-02 15:42:57 +0800

    Update main3.c

--- EX3/main3.c
+++ EX3/main3.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+int main(){
+    int i = 15;
+    if(i%15==0){
+        printf("Love IU");
+    }
+    else if(i%3==0){
+        printf("Love");
+    }
+    else if(i%5==0){
+        printf("IU");
+    }
+    else{
+        printf("%d\n", i);
+    }
+}
