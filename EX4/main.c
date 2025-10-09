3361f8e3159a2b2a07025280b7a789485ecadbf5
MaybeCandy01 <gary15878@gmail.com>
2025-10-09 16:19:00 +0800

    Add files via upload

--- EX4/main.c
+++ EX4/main.c
@@ -1,1 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main(){
+    FILE* fp;
+    int a[3] = {0,1,2};
+    char b[3] = "ABC";
+    float c[3] = {1.1,1.2,1.3};
+    int ra[3];
+    char rb[3];
+    float rc[3];
+   
+    fp = fopen("a.bin","wb+");
+    fwrite(a, sizeof(int), 3, fp);
+    fseek(fp, 0, SEEK_SET);
+    fread(ra, sizeof(int), 3, fp);
+    
+    fwrite(b, sizeof(char), 3, fp);
+    fseek(fp, sizeof(a), SEEK_SET);
+    fread(rb, sizeof(char), 3, fp);
+    
+    fwrite(c, sizeof(float), 3, fp);
+    fseek(fp, sizeof(a)+sizeof(b), SEEK_SET);
+    fread(rc, sizeof(float), 3, fp);
+
+    for(int i=0; i<3; i++){
+        printf("%d ",ra[i]);
+    }
+    printf("\n");
+    
+    for(int i=0; i<3; i++){
+        printf("%c ",rb[i]);
+    }
+    printf("\n");
+    
+    for(int i=0; i<3; i++){
+        printf("%f ",rc[i]);
+    }
+    printf("\n");
+    
+    
+    
+    
+
+    fclose(fp);
+    return 0;
+    
+}
